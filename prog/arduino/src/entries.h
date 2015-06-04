295d426bd5c268d3725ce15a7c499cd0d91c6aad
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-06-04 21:01:54 +0900

    arduino: 新しいヘッダの形式に対応

--- prog/arduino/src/entries.h
+++ prog/arduino/src/entries.h
@@ -4,7 +4,9 @@
 #include <stdint.h>
 #include <avr/pgmspace.h>
 
-#define read_entry(e, i, j) (pgm_read_byte(&(e[i][j])))
+#define read_entry_time(e, i) (pgm_read_dword(&(e[i])))
+#define read_entry_direction(e, i) (pgm_read_byte(&(e[i])))
+#define read_entry_degrees(e, i) (pgm_read_byte(&(e[i])))
 
 #include "entries_example.h"
 
